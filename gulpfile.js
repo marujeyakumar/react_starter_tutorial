90ddd501ad1d5352d513528202af4ad3e6299c9e
marujeyakumar <marudevijain@gmail.com>
2017-05-18 11:02:41 -0400

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,88 @@
+"use strict";
+
+var gulp = require('gulp');
+var connect = require('gulp-connect'); //Runs on a local dev server
+var open = require('gulp-open'); //Open a URL in a web browser
+var browserify = require('browserify'); //Bundles Js
+var reactify = require('reactify'); //Transforms react JSx to Js
+var source = require('vinyl-source-stream'); //Use conventional text streams with gulp 
+var concat = require('gulp-concat'); //Concatenates files
+var lint = require('gulp-eslint'); //Link Js files, including JSx
+
+
+
+var config = {
+    port: 9005,
+    devBaseUrl: 'http://localhost',
+    paths: {
+        html: './src/*.html',
+        js: './src/**/*.js',
+        images: './src/images/*',
+        css: [
+            'node_modules/bootstrap/dist/css/bootstrap.min.css',
+            'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
+        ],
+        dist: './dist', 
+        mainJs: './src/main.js'
+    }
+}
+
+//Start a local development server 
+gulp.task('connect', function() {
+    connect.server({
+        root: ['dist'],
+        port: config.port,
+        base: config.devBaseUrl,
+        libereload: true
+    });
+});
+
+gulp.task('open', ['connect'], function(){
+    gulp.src('dist/index.html')
+        .pipe(open({uri: config.devBaseUrl + ':' + config.port + '/'}));
+});
+
+gulp.task('html', function(){
+    gulp.src(config.paths.html)
+        .pipe(gulp.dest(config.paths.dist))
+        .pipe(connect.reload());
+});
+
+gulp.task('js', function(){
+    browserify(config.paths.mainJs)
+        .transform(reactify)
+        .bundle()
+        .on('error', console.error.bind(console))
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest(config.paths.dist + '/scripts'))
+        .pipe(connect.reload());
+});
+
+gulp.task('css', function(){
+    gulp.src(config.paths.css)
+        .pipe(concat('bundle.css'))
+        .pipe(gulp.dest(config.paths.dist + '/css'));
+});
+
+gulp.task('images', function(){
+    gulp.src(config.paths.images)
+        .pipe(gulp.dest(config.paths.dist + '/images'))
+        .pipe(connect.reload());
+
+    gulp.src('./src/favicon.ico')
+        .pipe(gulp.dest(config.paths.dist));
+});
+
+gulp.task('lint', function(){
+    return gulp.src(config.paths.js)
+        .pipe(lint({config: 'eslint.config.json'}))
+        .pipe(lint.format());
+})
+
+gulp.task('watch', function(){
+    gulp.watch(config.paths.html, ['html']);
+    gulp.watch(config.paths.js, ['js', 'lint']);
+});
+
+
+gulp.task('default', ['html','js','css','images','lint','open', 'watch']);
---
+++ src/api/authorData.js
@@ -0,0 +1,20 @@
+module.exports = {
+    authors: 
+    [
+        {
+            id: 'cory-house',
+            firstName: 'Cory',
+            lastName: 'House'
+        },
+        {
+            id: 'scott-allen',
+            firstName: 'Scott',
+            lastName: 'Allen'
+        },
+        {
+            id: 'dan-wahlin',
+            firstName: 'Dan',
+            lastName: 'Wahlin'
+        }
+    ]
+}; //each time we reload the app we will see this data 
